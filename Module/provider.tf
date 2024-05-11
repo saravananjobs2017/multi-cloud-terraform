957a5b66eb029b41daa4caa0872ec983832c843b
saravananjobs2017 <saravananjobs2017@gmail.com>
2024-05-11 13:46:10 +0530

    added new file

---
+++ Module/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+
+  required_version = ">= 0.14.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.25"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.17"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  profile    = "default"
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
